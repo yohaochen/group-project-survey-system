ab4410bf2fb3174d6ced5f47067db0fb21985fdd
yohaochen <haochen@upei.ca>
2022-02-11 17:24:58 +0800

    register backend finished

--- survey-system/routes/web.php
+++ survey-system/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
 Route::get('/login', function () {
     return view('auth.login');
 });
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+//Route::post('register_post', [UserController::class,'register_post');
+Route::get('/demo',[DemoController::class, 'demo']);
+
+Route::post('/register_post',[UserController::class, 'register_post']);
